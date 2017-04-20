a4e1397a895cf4f47cdd6506b354666f98885de3
Marco Bassaletti <bassaletti@gmail.com>
2017-04-20 00:37:53 -0300

    Skeleton SKU form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FormSkuComponent } from './form-sku/form-sku.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormSkuComponent
   ],
   imports: [
     BrowserModule,
