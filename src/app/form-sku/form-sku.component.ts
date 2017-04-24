4462ce2bed9563ddab3f40d30dbf9dfef64b4cbf
Marco Bassaletti <bassaletti@gmail.com>
2017-04-24 00:38:53 -0300

    Created form with FormBuilder.

--- src/app/form-sku/form-sku.component.ts
+++ src/app/form-sku/form-sku.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-sku',
@@ -7,12 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormSkuComponent implements OnInit {
 
-  constructor() { }
+  skuFormGroup: FormGroup;
+
+  constructor(formBuilder: FormBuilder) {
+    this.skuFormGroup = formBuilder.group({
+      'sku': ['ACB123'],
+      'name': ['Product Name']
+    });
+  }
 
   ngOnInit() {
   }
 
-  onSubmit(form: any): void {
+  onSubmit(form: any, event: Event): void {
+    event.preventDefault();
     console.log('submitted:');
     console.log(form);
   }
